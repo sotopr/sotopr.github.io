450b0036db81787b70dcc82a192d63cf03c6c402
sotopr <68881641+sotopr@users.noreply.github.com>
2020-07-27 21:40:01 -0400

    Files
    Upload Portfolio files

---
+++ projects/app.js
@@ -0,0 +1,170 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks);
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task events
+  clearBtn.addEventListener('click', clearTasks);
+  // filter tasks events
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Get Task from Local Storage
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+   
+  tasks.forEach(function(task){
+    // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(task));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class to link element
+  link.className = 'delete-item secondary-content';
+  // add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+  });
+}
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class to link element
+  link.className = 'delete-item secondary-content';
+  // add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Store in local storage
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Cleat input 
+  taskInput.value = '';
+
+// console.log(li);
+
+  e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+
+
+//Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      // Remove from Local storage
+      removeTaskFromLocalStorage
+      (e.target.parentElement.parentElement);
+      // console.log(e.target);
+    }
+  }
+}
+
+// Remmove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Clear tasks
+function clearTasks() {
+
+  //taskList.innerHTML = '';
+
+  // faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // Clear tasks from local storage
+  clearTasksFromLocalStorage();
+
+}
+
+// Clear tasks from local storage
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
+}
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
